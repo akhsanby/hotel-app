b1f460cd1c6275382a28f39a540a028e68f61e48
AkhsanBy <akhsanby@gmail.com>
2021-12-31 17:52:53 +0700

    add config to use the custom fonts

--- App.jsx
+++ App.jsx
@@ -1,9 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import Router from "./src/router";
+import AppLoading from "expo-app-loading";
+import { useFonts } from "expo-font";
 
 // utils
 import { color } from "./src/utils";
+
+let customFonts = {
+  "Outfit-Regular": require("./src/assets/fonts/Outfit-Regular.ttf"),
+  "Outfit-Bold": require("./src/assets/fonts/Outfit-Bold.ttf"),
+};
 
 const MyTheme = {
   ...DefaultTheme,
@@ -14,6 +21,12 @@ const MyTheme = {
 };
 
 export default function App() {
+  const [loaded] = useFonts(customFonts);
+
+  if (!loaded) {
+    return <AppLoading />;
+  }
+
   return (
     <NavigationContainer theme={MyTheme}>
       <Router />
