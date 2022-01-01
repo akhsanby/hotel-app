8f3d98b3897e2b3ea6dcb8f673479e04737bae0c
AkhsanBy <akhsanby@gmail.com>
2022-01-01 20:45:40 +0700

    add new component SmallCard

---
+++ src/components/SmallCard/styles.js
@@ -0,0 +1,37 @@
+import { StyleSheet } from "react-native";
+
+// utils
+import { color, windowWidth, windowHeight } from "../../utils";
+
+const styles = StyleSheet.create({
+  card: {
+    marginBottom: windowHeight * 0.02,
+    flexDirection: "row",
+  },
+  image: {
+    backgroundColor: color.whitePrimary,
+    width: windowWidth * 0.25,
+    height: windowWidth * 0.25,
+    borderRadius: 10,
+  },
+  card_body: {
+    paddingLeft: 10,
+    flex: 1,
+  },
+  text_name: {
+    color: color.whitePrimary,
+    fontSize: windowWidth * 0.04,
+    fontFamily: "Outfit-Bold",
+  },
+  text_location: {
+    color: color.whitePrimary,
+    fontSize: windowWidth * 0.03,
+    fontFamily: "Outfit-Regular",
+  },
+  star_icons: {
+    flexDirection: "row",
+    marginTop: windowHeight * 0.02,
+  },
+});
+
+export default styles;
