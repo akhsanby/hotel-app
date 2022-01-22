4592e1c3a150f16839f0f1a39bb2046292add578
AkhsanBy <akhsanby@gmail.com>
2022-01-22 16:53:11 +0700

    add loading indicator when searching

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { View, ScrollView, Text } from "react-native";
+import { ActivityIndicator, View, ScrollView, Text } from "react-native";
 
 // redux
 import { useSelector } from "react-redux";
@@ -9,27 +9,35 @@ import SearchForm from "../../components/SearchForm";
 import BigCard from "../../components/BigCard";
 import Gap from "../../components/Gap";
 
+// utils 
+import { color } from '../../utils'
+
 // styles
 import styles from "./styles";
 
 export default function Search({ navigation }) {
   const searchResult = useSelector((state) => state.hotel.searchResult);
+  const isFetchLoading = useSelector((state) => state.hotel.loading);
 
   return (
     <View style={styles.container}>
       <Gap height={40} />
       <SearchForm />
       <Gap height={10} />
-      {searchResult.length > 0 ? (
-        <ScrollView showsVerticalScrollIndicator={false}>
-          {searchResult?.map((searchResult, index) => (
-            <BigCard key={index} navigation={navigation} hotel={searchResult} />
-          ))}
-        </ScrollView>
+      {isFetchLoading ? (
+        <ActivityIndicator size="large" color={color.red} />
       ) : (
-        <Text style={styles.text_notfound}>
-          Hotel is not found! try other keyword
-        </Text>
+        searchResult.length > 0 ? (
+          <ScrollView showsVerticalScrollIndicator={false}>
+            {searchResult?.map((searchResult, index) => (
+              <BigCard key={index} navigation={navigation} hotel={searchResult} />
+            ))}
+          </ScrollView>
+        ) : (
+          <Text style={styles.text_notfound}>
+            Hotel is not found! try other keyword
+          </Text>
+        )
       )}
     </View>
   );
